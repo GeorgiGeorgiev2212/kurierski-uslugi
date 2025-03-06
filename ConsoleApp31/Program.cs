10093960798febc2a6d4cea1ea12f5183f6cb4b3
Georgi Georgiev <georgievg2212@icloud.com>
2025-03-06 15:14:31 +0200

    Add project files.

---
+++ ConsoleApp31/Program.cs
@@ -0,0 +1,26 @@
+﻿double money = 0;
+Console.WriteLine("Vuvedi sumata, koqto shte izprashtate: ");
+double pari = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the country you want to ship to: ");
+string country = Console.ReadLine();
+
+if (country == "EU")
+{
+
+    money = 10.50 + pari;
+    Console.WriteLine($"The charge is {money}  euro");
+
+}
+
+else if (country == "GB")
+{
+
+    money = 23.00 + pari;
+    Console.WriteLine($"The charge is {money} euro");
+}
+else
+{
+    money = 20.00 + pari;
+    Console.WriteLine($"The charge is {money} euro");
+
+}
